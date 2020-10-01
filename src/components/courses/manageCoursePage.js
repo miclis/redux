c546dc58cb81a4bd9efb899d0797f290fbf676a0
mxlisows <mxlisows@molsoncoors.com>
2020-10-02 00:13:50 +0200

    Add course functionality implemented

---
+++ src/components/courses/manageCoursePage.js
@@ -0,0 +1,62 @@
+ddimport React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
+import PropTypes from 'prop-types';
+import CourseForm from './CourseForm';
+import { newCourse } from '../../../tools/mockData';
+
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, saveCourse, ...props }) {
+    const [course, setCourse] = useState({ ...props.course });
+    const [errors, setErrors] = useState({});
+
+    useEffect(() => {
+        if (courses.length === 0) {
+            loadCourses().catch((error) => {
+                alert('Loading courses failed ' + error);
+            });
+        }
+        if (authors.length === 0) {
+            loadAuthors().catch((error) => {
+                alert('Loading authors failed ' + error);
+            });
+        }
+    }, []);
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setCourse((prevCourse) => ({ ...prevCourse, [name]: name === 'authorId' ? parseInt(value, 10) : value }));
+    }
+
+    function handleSave(event) {
+        event.preventDefault();
+        saveCourse(course);
+    }
+
+    return <CourseForm course={course} errors={errors} authors={authors} onChange={handleChange} onSave={handleSave} />;
+}
+
+ManageCoursePage.propTypes = {
+    course: PropTypes.object.isRequired,
+    courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired,
+    loadCourses: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired,
+    saveCourse: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+    return {
+        course: newCourse,
+        courses: state.courses,
+        authors: state.authors,
+    };
+}
+
+const mapDispatchToProps = {
+    loadCourses: courseActions.loadCourses,
+    loadAuthors: authorActions.loadAuthors,
+    saveCourse: courseActions.saveCourse,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
