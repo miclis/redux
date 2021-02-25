a6a07d16018b4ac3d50496d422aa3c7fc2f4f4e2
michal.lisowski <michal.lisowski@finally.global>
2021-02-25 14:02:07 +0100

    Typo fix

--- src/components/App.js
+++ src/components/App.js
@@ -10,20 +10,20 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-    return (
-        <div className="container-fluid">
-            <Header />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/about" component={AboutPage} />
-                <Route path="/courses" component={CoursesPage} />
-                <Route path="/course/:slug" component={ManageCoursePage} />
-                <Route path="/course" component={ManageCoursePage} />
-                <Route component={PageNotFound} />
-            </Switch>
-            <ToastContainer autoClose={3000} />
-        </div>
-    );
+	return (
+		<div className='container-fluid'>
+			<Header />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/about' component={AboutPage} />
+				<Route path='/courses' component={CoursesPage} />
+				<Route path='/course/:slug' component={ManageCoursePage} />
+				<Route path='/course' component={ManageCoursePage} />
+				<Route component={PageNotFound} />
+			</Switch>
+			<ToastContainer autoClose={3000} />
+		</div>
+	);
 }
 
 export default App;
